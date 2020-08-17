8ff35d3c80913665165d8690c555ecce0c992c10
Bicky <bicky@senseforth.com>
2020-08-17 21:37:06 +0530

    CHaracter Recognition

---
+++ OpticalCharacterRecognition/src/main/java/com/springboot/Test.java
@@ -0,0 +1,15 @@
+package com.springboot;
+
+import java.io.File;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+public class Test {
+
+	public static void main(String[] args) throws TesseractException {
+		Tesseract tesseract = new Tesseract();
+		tesseract.setDatapath("/home/bicky/Pictures/Opc/eng.traineddata"); ///
+		System.out.println(tesseract.doOCR(new File("home/bicky/Downloads/teseract.jpg")));
+	}
+}
